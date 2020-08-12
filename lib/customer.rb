589ac1faff0cc59d2cc6c80e4df118967603f688
Kyle Stough <kstough00@gmail.com>
2020-08-12 18:21:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,9 +19,15 @@ class Waiter
   end
   
   def meals
-  Meal.all.select do |meal|
+    Meal.all.select do |meal|
     meal.customer == self
+    end
   end
-end
+  
+  def waiters
+    meals.map do |meal|
+    meal.waiter
+    end
+  end
  
 end
