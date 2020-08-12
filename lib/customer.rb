02ab8fe1a9154be22b1bb8ccd483fe90de34f44e
Kyle Stough <kstough00@gmail.com>
2020-08-12 18:16:35 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,18 +1,17 @@
-class Meal
+class Waiter
  
-  attr_accessor :waiter, :customer, :total, :tip
+  attr_accessor :name, :yrs_experience
  
   @@all = []
  
-  def initialize(waiter, customer, total, tip=0)
-    @waiter = waiter
-    @customer = customer
-    @total = total
-    @tip = tip
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
     @@all << self
   end
  
   def self.all
     @@all
   end
+ 
 end
