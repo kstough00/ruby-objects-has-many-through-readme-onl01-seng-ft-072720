f70943744d1f0e5852fb2c4c4c564ff826414ba1
Kyle Stough <kstough00@gmail.com>
2020-08-12 18:14:34 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,12 @@
-class Customer
-  attr_accessor :name, :age
+class Waiter
+ 
+  attr_accessor :name, :yrs_experience
  
   @@all = []
  
-  def initialize(name, age)
+  def initialize(name, yrs_experience)
     @name = name
-    @age = age
+    @yrs_experience = yrs_experience
     @@all << self
   end
  
